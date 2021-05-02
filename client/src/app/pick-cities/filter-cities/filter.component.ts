e31c2f00204b2a0d0a8c741e2fd91e72bc0b217d
gowesoft <gowesoft@gmail.com>
2021-05-02 20:46:54 -0300

    Added list and services

---
+++ client/src/app/pick-cities/filter-cities/filter.component.ts
@@ -0,0 +1,11 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Component({
+    selector: 'app-filter-cities',
+    templateUrl: './filter.component.html',
+    styleUrls: ['./filter.component.scss']
+})
+export class FilterComponent {
+    constructor() { }
+}
